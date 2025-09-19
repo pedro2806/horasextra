2364daed3638c6913b1848df9436232b77d59363
pedro2806 <isc.pedro.mtz@gmail.com>
2025-09-19 10:16:17 -0600

    Funcionalidades de login master
    Funcionalidades de login master

--- logout.php
+++ logout.php
@@ -17,15 +17,30 @@
 	<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.print.min.js"></script>
 	<script type="text/javascript" class="init">
 	
-$(document).ready(function() {
-    document.cookie = "noEmpleado =00; expires=Thu, 01 Jan 1970 00:00:00 UTC";
-    document.cookie = "nombre =00; expires=Thu, 01 Jan 1970 00:00:00 UTC";
-    document.cookie = "region =00; expires=Thu, 01 Jan 1970 00:00:00 UTC";
-    document.cookie = "rol =00; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+		$(document).ready(function() {
+			document.cookie = "noEmpleado =00; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+			document.cookie = "nombre =00; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+			document.cookie = "region =00; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+			document.cookie = "rol =00; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 
-    window.location.assign("https://www.messbook.com.mx/horasextra")        
-} );
-
+			let cookieSesion = getCookie("SesionLogin");
+							//alert("Cerrando sesi贸n..." + cookieSesion);
+			if (cookieSesion === "LoginMaster") {
+				document.cookie = "SesionLogin=00; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+				window.location.assign("../loginMaster/inicio");
+			} else {
+				window.location.assign("https://www.messbook.com.mx/horasextra/");
+			}
+		} );
+	
+	
+		//Funcion para leer cookies
+		function getCookie(name) {
+			let value = "; " + document.cookie;
+			let parts = value.split("; " + name + "=");
+			if (parts.length === 2) return parts.pop().split(";").shift();
+			return null; // Si no encuentra la cookie, retorna null
+		}
 	</script>
 
 </head>
---
+++ validaLoginMaster.php
@@ -0,0 +1,55 @@
+<?php
+include 'conn.php';
+
+$accion = isset($_POST['accion']) ? $_POST['accion'] : '';
+
+// Validación de usuario (otro caso)
+$id_usuario = isset($_POST['id_usuarioHR']) ? $_POST['id_usuarioHR'] : '';
+$nombredelusuario = isset($_POST['nombredelusuarioHR']) ? $_POST['nombredelusuarioHR'] : '';
+$noEmpleado = isset($_POST['noEmpleadoHR']) ? $_POST['noEmpleadoHR'] : '';
+$rol = isset($_POST['rolHR']) ? $_POST['rolHR'] : '';
+$usuario = isset($_POST['correoHR']) ? $_POST['correoHR'] : '';
+
+
+    $Qempresas  =  "SELECT  *, TIMESTAMPDIFF(YEAR,fechaIngreso,CURDATE()) AS antiguedad, rol FROM usuarios WHERE usuario  = '".$usuario."' AND estatus = 1";
+    $res2 =  mysqli_query( $conn, $Qempresas ) or die (mysqli_error($conn));
+    $nr = mysqli_num_rows($res2);
+
+    while ($row2 = mysqli_fetch_array($res2)){
+        $id_usuario = $row2["id"];
+        $nombreEmpleado = $row2["nombre"];
+        $noEmpleado = $row2["noEmpleado"];
+        $antiguedad = $row2["antiguedad"];
+        $diasD = $row2["diasdisponibles"];
+        $rol = $row2["rol"];
+    }
+
+    if($nr == 1){
+        
+        echo '<script>document.cookie = "nombre='.$nombreEmpleado.';expires=" + new Date(Date.now() + 99900000).toUTCString() + ";SameSite=Lax;";</script>';
+        echo '<script>document.cookie = "region='.$region.';expires=" + new Date(Date.now() + 99900000).toUTCString() + ";SameSite=Lax;";</script>';
+        echo '<script>document.cookie = "noEmpleado='.$noEmpleado.';expires=" + new Date(Date.now() + 99900000).toUTCString() + ";SameSite=Lax;";</script>';
+        echo '<script>document.cookie = "area='.$area.';expires=" + new Date(Date.now() + 99900000).toUTCString() + ";SameSite=Lax;";</script>';
+        echo '<script>document.cookie = "rol='.$rol.';expires=" + new Date(Date.now() + 99900000).toUTCString() + ";SameSite=Lax;";</script>';
+        echo '<script>document.cookie = "SesionLogin=LoginMaster; expires=" + new Date(Date.now() + 99900000).toUTCString() + ";SameSite=Lax;";</script>';
+        echo '<script>window.location.assign("inicio")</script>';
+
+        session_start();
+        $_SESSION['nombredelusuario'] = $nombreEmpleado;
+        $_SESSION['noEmpleado'] = $noEmpleado;
+        $_SESSION['rol'] = $rol;
+        $_SESSION['correo'] = $usuario;
+        $_SESSION['id_usuario'] = $id_usuario;
+
+        echo json_encode(['success' => true]);
+        exit;
+    }
+
+    // Si no hay usuario válido
+    echo json_encode([
+        'success' => false,
+        'message' => 'Usuario no válido.',
+    ]);
+    exit;
+
+?>
